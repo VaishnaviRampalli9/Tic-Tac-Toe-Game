37e545199fc6c5778099d421347166ee4a2958cd
VaishnaviRampalli9 <vaishnavirampalli@gmail.com>
2024-10-12 19:11:35 +0530

    initial-commit
    Initial commit of Tic-Tac-Toe project

---
+++ src/utils/game-utils.js
@@ -0,0 +1,46 @@
+import WINNING_COMBINATIONS from "./winning-combinations.js";
+
+const INITIAL_GAME_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export function deriveWinner(gameBoard, players) {
+  let winner = null;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].col];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      secondSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = players[firstSquareSymbol];
+      break;
+    }
+  }
+  return winner;
+}
+
+export function deriveActivePlayer(gameTurns) {
+  return gameTurns.length > 0 && gameTurns[0].player === "X" ? "O" : "X";
+}
+
+export function deriveGameBoard(gameTurns) {
+  let gameBoard = [...INITIAL_GAME_BOARD.map((row) => [...row])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
+}
+
+export const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2",
+};
